9fc0bf1acf252c2b2cc3d916e923b63447a3e551
unknown <juha.salakka1@gmail.com>
2020-04-06 12:44:53 +0300

    uibuild

---
+++ build/precache-manifest.dfc0f52e9755c760601df109381187c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ce914ba9f36b8993599225095f0ede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9442834ea956886db482",
+    "url": "/static/css/main.9331e924.chunk.css"
+  },
+  {
+    "revision": "c3f9620062117e9fbe68",
+    "url": "/static/js/2.39b03661.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39b03661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9442834ea956886db482",
+    "url": "/static/js/main.4764baa6.chunk.js"
+  },
+  {
+    "revision": "e1bf988eeb9b3e497aa7",
+    "url": "/static/js/runtime-main.72ab343c.js"
+  }
+]);
